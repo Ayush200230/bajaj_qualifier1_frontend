594266b08eccda46c3f14a1e327902ea2b77f401
Ayush200230 <humbleayush30@gmail.com>
2024-08-02 19:14:54 +0530

    Fixing design

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Home() {
     return (
         <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-            <div>RA2111003011823</div>
+            <div className='text-6xl font-extrabold'>RA2111003011823</div>
             <div>
                 <ApiForm />
             </div>
